6cfbae650c1e3965e6d052c89abae0d634d77d8b
markoopoloo <mjpaivarinta@gmail.com>
2016-02-03 12:30:17 +0200

    Not all files were updated.

---
+++ Image_Sharing_Site/App_Code/BusinessLayer/uploadedImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Image_Sharing_Site.App_Code.BusinessLayer
+{
+    public class uploadedImage
+    {
+        private int imageID;
+        private string imagePath;
+        private int commentID;
+
+        public uploadedImage()
+        {
+            
+        }
+
+        public int ImageID
+        {
+            get { return imageID; }
+            set { imageID = value; }
+        }
+
+        public String ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+
+        public int CommentID
+        {
+            get { return commentID; }
+            set { commentID = value; }
+        }
+
+    }
+}
